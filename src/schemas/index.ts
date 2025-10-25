110a4071a3a347ea4d86cfd7aec4d6c5f783139b
Joan Xie <joan@anthropic.com>
2025-10-24 20:52:58 -0700

    remove import

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,5 @@
 export * as v0_1 from "./0.1.js";
 export * as v0_2 from "./0.2.js";
-export * as vNext from "./vNext.js";
 export * as latest from "./latest.js";
 export {
   MANIFEST_VERSION as LATEST_MANIFEST_VERSION,
